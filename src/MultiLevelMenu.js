ef804a44db02f2681ed90878170909a65bdd418a
Saravana Kumar <saravana.kumar@drylogics.com>
2018-12-15 18:16:17 +0530

    Files aligned

--- src/MultiLevelMenu.js
+++ src/MultiLevelMenu.js
@@ -6,45 +6,58 @@ const Item = props => {
   const { item, displaySubmenu, itemClicked } = props;
   const name = item.name;
   return (
-    <div className={`item ${name}`} onClick={() => itemClicked(name)} >
+    <div className={`item ${name}`} onClick={() => itemClicked(name)}>
       <label className={`label ${name}`}>{name}</label>
-      {
-        (!_.isUndefined(item.subitems)&&<span onMouseEnter={() => displaySubmenu(item.subitems)}>&#x25BA;</span>)
-      }
+      {!_.isUndefined(item.subitems) && (
+        <span onMouseEnter={() => displaySubmenu(item.subitems)}>&#x25BA;</span>
+      )}
     </div>
   );
 };
 
 const Group = props => {
-  const { name, items,displaySubmenu,itemClicked } = props;
+  const { name, items, displaySubmenu, itemClicked } = props;
   // const sortedItems = _.sortBy(items);
   const sortedItems = _.orderBy(items, "name");
   return (
     <div className={`group ${name}`}>
       {_.map(sortedItems, item => {
-        return <Item item={item} displaySubmenu={displaySubmenu} itemClicked={itemClicked}/>;
+        return (
+          <Item
+            item={item}
+            displaySubmenu={displaySubmenu}
+            itemClicked={itemClicked}
+          />
+        );
       })}
     </div>
   );
 };
 
 const MenuContainer = props => {
-  const {data, displaySubmenu,itemClicked} = props;
-  return(
+  const { data, displaySubmenu, itemClicked } = props;
+  return (
     <div className="container">
       {_.map(data, group => {
-        return <Group name="group 3" items={group} displaySubmenu={displaySubmenu} itemClicked={itemClicked}/>;
+        return (
+          <Group
+            name="group 3"
+            items={group}
+            displaySubmenu={displaySubmenu}
+            itemClicked={itemClicked}
+          />
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
 class MultiLevelMenu extends Component {
   constructor(props) {
     super(props);
-    const {data} = props;
+    const { data } = props;
     const levelArray = [];
-    levelArray.push(data)
+    levelArray.push(data);
     this.state = {
       data: data,
       levelArray: levelArray,
@@ -53,24 +66,24 @@ class MultiLevelMenu extends Component {
   }
 
   displaySubmenu = subitems => {
-    const levelArray = this.state.levelArray
+    const levelArray = this.state.levelArray;
     // const updatedLevelArray = levelArray
-    if(!_.includes(levelArray,subitems)){
-      levelArray.push(subitems)
+    if (!_.includes(levelArray, subitems)) {
+      levelArray.push(subitems);
     }
     this.setState({
-      levelArray: this.state.levelArray,
+      levelArray: this.state.levelArray
     });
-  }
+  };
 
   itemClicked = item => {
     const levelArray = [];
-    levelArray.push(this.state.data)
+    levelArray.push(this.state.data);
     this.setState({
       selectedOption: item,
       levelArray: levelArray
     });
-  }
+  };
 
   render() {
     const displaySubmenu = this.displaySubmenu;
@@ -78,11 +91,15 @@ class MultiLevelMenu extends Component {
     return (
       <div className="App">
         <div class="test">Selected Option: {this.state.selectedOption}</div>
-        {
-          _.map(this.state.levelArray, data => {
-            return <MenuContainer data={data} displaySubmenu={displaySubmenu} itemClicked={itemClicked}/>
-          })
-        }
+        {_.map(this.state.levelArray, data => {
+          return (
+            <MenuContainer
+              data={data}
+              displaySubmenu={displaySubmenu}
+              itemClicked={itemClicked}
+            />
+          );
+        })}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,73 +4,76 @@ import "./index.css";
 import MultiLevelMenu from "./MultiLevelMenu";
 
 {
-  const data = [
-    [
-      {
-        name: "item z",
-        subitems: [
-          [{ name: "item z-1" }, { name: "item z-2" }],
-          [{ name: "item z-3" }]
-        ]
-      },
-      {
-        name: "item y"
-      },
-      {
-        name: "item x"
-      },
-      {
-        name: "item v"
-      }
-    ],
-    [
-      {
-        name: "item d",
-        subitems: [
-          [{ name: "item d-1" }],
-          [{ name: "item d-2" }, { name: "item d-3", subitems:[
-						[{ name: "item d-3-1" }],
-						[{ name: "item d-3-2",
-						subitems: [
-							[{ name: "item d-3-2-1" }, { name: "item d-3-2-2" }],
-							[{ name: "item d-3-2-3" }]
-						]
-					 }, { name: "item d-3-3" }]
-					] }]
-        ]
-      },
-      {
-        name: "item b"
-      },
-      {
-        name: "item a"
-      },
-      {
-        name: "item c"
-      }
+	const data = [
+		[
+			{
+				name: "item z",
+				subitems: [
+					[{ name: "item z-1" }, { name: "item z-2" }],
+					[{ name: "item z-3" }]
+				]
+			},
+			{
+				name: "item y"
+			},
+			{
+				name: "item x"
+			},
+			{
+				name: "item v"
+			}
 		],
 		[
-      {
-        name: "item g",
-        subitems: [
-          [{ name: "item a-1" }, { name: "item a-2" }],
-          [{ name: "item a-3" }]
-        ]
-      },
-      {
-        name: "item f"
-      },
-      {
-        name: "item i"
-      },
-      {
-        name: "item h"
-      }
-    ]
-  ];
+			{
+				name: "item d",
+				subitems: [
+					[{ name: "item d-1" }],
+					[{ name: "item d-2" }, {
+						name: "item d-3", subitems: [
+							[{ name: "item d-3-1" }],
+							[{
+								name: "item d-3-2",
+								subitems: [
+									[{ name: "item d-3-2-1" }, { name: "item d-3-2-2" }],
+									[{ name: "item d-3-2-3" }]
+								]
+							}, { name: "item d-3-3" }]
+						]
+					}]
+				]
+			},
+			{
+				name: "item b"
+			},
+			{
+				name: "item a"
+			},
+			{
+				name: "item c"
+			}
+		],
+		[
+			{
+				name: "item g",
+				subitems: [
+					[{ name: "item a-1" }, { name: "item a-2" }],
+					[{ name: "item a-3" }]
+				]
+			},
+			{
+				name: "item f"
+			},
+			{
+				name: "item i"
+			},
+			{
+				name: "item h"
+			}
+		]
+	];
 
-  ReactDOM.render(
-    <MultiLevelMenu data={data} />,
-    document.getElementById("root")
-  );
+	ReactDOM.render(
+		<MultiLevelMenu data={data} />,
+		document.getElementById("root")
+	);
 }
